944205f7cf89643569b4cee140739ea0abc07914
OsadchiyO <olegtruefalse@gmail,com>
2015-09-17 21:19:46 +0300

    Hello World commit

---
+++ hillel/src/MyFirstClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by OsadchiyO on 17.09.2015.
+ */
+public class MyFirstClass {
+    public static void main(String[] args){
+        String helloMessage = "Hello World!!!!";
+        System.out.println(helloMessage );
+
+        int age = 18;
+        final int year = 1997;
+        System.out.println("I am " + age);
+        System.out.println(32+age);
+
+    }
+}
