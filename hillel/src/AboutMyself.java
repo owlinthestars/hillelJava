dbfc782bebcf4fb6976b2e6af9dfb18e3166fdc8
Oleg <olegtruefalse@gmail.com>
2015-09-20 15:48:14 +0400

    Homework Aboutmyself version 2.0

--- hillel/src/AboutMyself.java
+++ hillel/src/AboutMyself.java
@@ -5,11 +5,18 @@ public class AboutMyself {
     public static void main (String [] args){
         String myName = "Oleg";
         String fullName = "Osadchiy";
+        final String green = ((char)27+"[32m");
+        final String black = ((char)27+"[30m");
+        final String red = ((char)27+"[31m");
+        final String yellow = ((char)27+"[33m");
+        final String blue = ((char)27+"[34m");
+        final String blue2 = ((char)27+"[36m");
+
 
         /*I wanted a little variety - find info about colors*/
-        System.out.println((char)27 + "[31mInformation about myself:" +(char)27 +  "[0m");
+        System.out.println(red + "Information about myself:"  );
 
-        System.out.println((char)27 + "[32mStudent fullname: " +(char)27 +"[0m" + myName + " " + fullName);
+        System.out.println(green + "Student fullname: "  + black + ( myName + " " + fullName));
 
         final int bornYear = 1997;
         int todayYear = 2015;
@@ -17,10 +24,10 @@ public class AboutMyself {
 
         /*This way for displaying version w/o third partially*/
 
-        System.out.println ((char)27 + "[34mMy age without third Partially: " +(char)27 +"[0m"+ (todayYear - bornYear));
+        System.out.println (blue + "My age without third Partially: " + black + (todayYear - bornYear));
 
         /*For rationality and full complete the task - displaying third partially*/
-        System.out.println ((char)27 + "[33mMy age with third Partially: " +(char)27 +"[0m"+ (myAge));
+        System.out.println (yellow +"My age with third Partially: " + black + (myAge));
 
     }
 
