bee68a336caa110be40e18e52f05f70665b3c0f3
Oleg <olegtruefalse@gmail.com>
2015-09-28 16:05:27 +0400

    Homework workwithnumbers

--- hillel/src/WorkWithNumbers.java
+++ hillel/src/WorkWithNumbers.java
@@ -6,6 +6,32 @@ import java.util.Scanner;
 public class WorkWithNumbers {
     public static void main(String[] args){
         checkNumbers();
+        nearestNumber();
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter area of a rectangle ");
+        int area = scanner.nextInt();
+        System.out.print("Please enter Perimeter of a rectangle ");
+        int Perimeter = scanner.nextInt();
+
+        int a = (int) ((Math.sqrt(Math.pow(Perimeter, 2) - 16 * area) + Perimeter) / 4);
+
+        int b = (area / a);
+
+        if (2 * a + 2 * b == Perimeter) {
+            System.out.println("First sides" + a );
+            System.out.println("Second sides = " + b);
+
+        } else if(a==b){
+            System.out.println("Your figure is square");
+
+        } else{
+            System.out.println("Rectangle with this parameters cant exist");
+        }
+
+    }
+
+    private static void nearestNumber() {
         Scanner scanner = new Scanner(System.in);
 
 
@@ -29,6 +55,7 @@ public class WorkWithNumbers {
             System.out.println("Second number nearest ");
         }
     }
+
     private static void checkNumbers() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is your number?: ");
