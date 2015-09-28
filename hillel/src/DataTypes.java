37867ffbcb04b860815c26cc802ce4da035aeec3
OsadchiyO <olegtruefalse@gmail,com>
2015-09-28 21:05:39 +0300

    Work with operations and something else

--- hillel/src/DataTypes.java
+++ hillel/src/DataTypes.java
@@ -3,6 +3,40 @@
  */
 public class DataTypes {
     public static void main (String[] args) {
+        byte b = 120;
+        short s = b;
+        long l = s;
+
+        double d = l; //Возможна потеря точности.Missing tochnost'
+
+        l = 15;
+        int maxValue  = Integer.MAX_VALUE;
+        System.out.println("Max value " + maxValue);
+        long intMaxValue = maxValue;
+        long intOverflow = intMaxValue + 1;
+        int i = (int) intOverflow;//Удаляет старшие биты. bad idea
+        System.out.println("overflow " + i);
+        d = Math.PI;
+
+        i = (int) d;
+        System.out.println(i);
+
+
+
+
+    }
+
+
+
+    private static void realNumberComparison() {
+        if(1.1 + 2.2 == 3.3){
+            System.out.println("they are equal");
+        }else {
+            System.out.println("they aren't equal");
+        }
+    }
+
+    private static void primitiveDataTypes() {
         int days = 365;
         int hours = days * 24;
         int seconds = hours * 3600;
--- hillel/src/WorkflowControl.java
+++ hillel/src/WorkflowControl.java
@@ -4,48 +4,110 @@ import java.util.Scanner;
  * Created by OsadchiyO on 24.09.2015.
  */
 public class WorkflowControl {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        ternariExample();
+
+    }
+
+    private static void ternariExample() {
+        int number = 5;
+        String isEven = isEven(number);
+
+        isEven =  (number % 2 == 0) ? "yes" : "no"; //Тернарный оператор. Ternarnii Operator (sokr if else)
+    }
+
+    public static String isEven(int someNumber){
+    if(someNumber % 2 == 0){
+        return "yes";
+    } else {
+        return "no";
+    }
+
+}
+    private static void switchCaseSecondExample() {
+        String month = "mar";
+
+        switch(month) {
+
+            case "jan":
+            case "feb":
+            case "dec":
+                System.out.println("winter");
+                break;
+            case "mar":
+            case "apr":
+            case "may":
+                System.out.println("spring");
+                break;
+            default:
+                System.out.println("wrong month name" + month);
+                break;
+        }
+    }
+
+    private static void switchCaseExample(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter month number");
+        int month = scanner.nextInt();
+        switch (month){
+            case 1:
+                System.out.println("jan");
+                break;
+            case 5:
+                System.out.println("may");
+                break;
+            case 12:
+                System.out.println("dec");
+                break;
+            default:
+                System.out.println("Wrong month number");
+                break;
+            case 2:
+                System.out.println("feb");
+                break;
+        }
+    }
+
+    private static void ifExample(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter here your age: ");
 
-      int age = scanner.nextInt();
+        int age = scanner.nextInt();
 /*if and else */
-        if (age <20){
+        if (age < 20) {
             System.out.println("You are young");
-        } else{
-            if(age<15){
+        } else {
+            if (age < 15) {
                 System.out.println("You are a little boy/girl");
-            }else
-            {
-                if (age<50){
+            } else {
+                if (age < 50) {
                     System.out.println("You are not so young");
-                }else {
+                } else {
                     System.out.println("you are oldman");
                 }
             }
         }
 /*A lot of if*/
-        if (age >= 0 && age <10){
+        if (age >= 0 && age < 10) {
             System.out.println("kid");
         }
-        if (age >=10 && age < 20){
+        if (age >= 10 && age < 20) {
             System.out.println("teen");
         }
-        if (age >=20 && age < 50){
+        if (age >= 20 && age < 50) {
             System.out.println("adult");
         }
-        if (age >=50){
+        if (age >= 50) {
             System.out.println("old");
         }
     }
 
     private static void ifExplanation() {
         boolean condition = true;
-        if(condition) {
+        if (condition) {
             System.out.println("in IF");
             System.out.println("still in IF block");
-        }
-        else {
+        } else {
             System.out.println("Out of IF block");
         }
     }
