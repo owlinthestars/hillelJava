80e085d0dc86472d0ec69d9d495ee363ffc0db83
Oleg Osadchiy <olegtruefalse@gmail.com>
2015-09-28 16:08:13 +0400

    Update WorkWithNumbers.java

--- hillel/src/WorkWithNumbers.java
+++ hillel/src/WorkWithNumbers.java
@@ -36,23 +36,23 @@ public class WorkWithNumbers {
 
 
         System.out.print("Enter your first number: ");
-        int number1 = scanner.nextInt();
+        int firstNumber = scanner.nextInt();
 
         System.out.print("Enter your second number: ");
-        int number2 = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
 
-        int constantNumber = 10;
-        int res1 = constantNumber - number1;
+        int constNumber = 10;
+        int res1 = constNumber - firstNumber;
 
 
-        int res2 = constantNumber - number2;
+        int res2 = constNumber - secondNumber;
 
         if (res1 < res2) {
-            System.out.println("First number nearest");
+            System.out.println("First number is nearest");
         } else if (res1 == res2) {
-            System.out.println("They also nearest");
+            System.out.println("They also are nearest");
         } else {
-            System.out.println("Second number nearest ");
+            System.out.println("Second number is nearest ");
         }
     }
 
