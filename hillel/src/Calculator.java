fee5b183e56ba1ceb5db72aff1647477c1d1a543
Oleg <olegtruefalse@gmail.com>
2015-09-30 00:20:11 +0400

    Calculator another version

--- hillel/src/Calculator.java
+++ hillel/src/Calculator.java
@@ -1,54 +1,135 @@
 import java.util.Scanner;
+
 /**
  * Created by Oleg on 29.09.2015.
  *
  */
 public class Calculator {
     public static void main(String[] args) {
+        typeChecker();
 
+
+    }
+
+    private static void typeChecker() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        int b = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter what type you want to check: ");
+
+        String var = scanner.nextLine();
+
+        if (b >= (-128) || b <= 127) {
+            System.out.println("this number can be byte");
+        } else {
+            System.out.println("This number cant be byte");
+        }
+        if (b >= -32768 || b <= 32767) {
+            System.out.println("this number can be short");
+        } else {
+            System.out.println("This number cant be short");
+        }
+        if (b >= -2147483648 || b <= 2147483647) {
+            System.out.println("this number can be int");
+        } else {
+            System.out.println("This number cant be int");
+        }
+        if (b >= -9223372036854775808L || b <= 9223372036854775807L) {
+            System.out.println("this number can be long");
+        } else {
+            System.out.println("This number cant be long");
+        }
+        /*
+        switch (var) {
+            case "byte":
+                if (b >= (-128) || b <= 127) {
+                    System.out.println("this number can be byte");
+                } else {
+                    System.out.println("This number cant be byte");
+                }
+                break;
+            case "short":
+                if (b >= -32768 || b <= 32767) {
+                    System.out.println("this number can be short");
+                } else {
+                    System.out.println("This number cant be short");
+                }
+                break;
+            case "int":
+                if (b >= -2147483648 || b <= 2147483647) {
+                    System.out.println("this number can be int");
+                } else {
+                    System.out.println("This number cant be int");
+                }
+                break;
+            case "long":
+                if (b >= -9223372036854775808L || b <= 9223372036854775807L) {
+                    System.out.println("this number can be long");
+                } else {
+                    System.out.println("This number cant be long");
+                }
+                break;
+        }*/
+    }
+
+    private static void mediumOperation() {
+        double res;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number a: ");
+        double a = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Enter operation what you want to do: ");
+        String operation = scanner.nextLine();
+        switch(operation){
+            case "trunk":
+                 res = (int) a;
+                System.out.println((int)res);
+                break;
+            case "fract":
+                res = a -( (int) a);
+                System.out.println((float)res);
+                break;
+
+
+        }
+    }
+
+    private static void easyOperation() {
+        double res;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your a ");
         double a = scanner.nextDouble();
         System.out.println("Enter your b ");
         double b = scanner.nextDouble();
+        scanner.nextLine();
         System.out.println("Enter what operation you want to do:");
         String operation = scanner.nextLine();
         switch (operation) {
             case "+":
-                double res = a + b;
+            case "plus":
+                 res = a + b;
                 System.out.println(res);
                 break;
             case "-":
-                res = a - b;
-                System.out.println(res);
-                break;
-            case "*":
-                res = a * b;
-                System.out.println(res);
-                break;
-            case "/":
-                res = a / b;
-                System.out.println(res);
-                break;
-            case "plus":
-                res = a + b;
-                System.out.println(res);
-                break;
             case "minus":
                 res = a - b;
                 System.out.println(res);
                 break;
+            case "*":
             case "increase":
                 res = a * b;
                 System.out.println(res);
                 break;
+            case "/":
             case "divide":
                 res = a / b;
                 System.out.println(res);
                 break;
-
+            default:
+                System.out.println("Not easy operation for me. I'm sorry");
+                break;
         }
-
-
     }
 }
